7accb1f3ad5f56953fc766a119b883026742fbff
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-17 10:12:46 -0300

    modified: getList method implemented in DataConverter class

--- src/main/java/br/com/alura/TabelaFipe/service/DataConverter.java
+++ src/main/java/br/com/alura/TabelaFipe/service/DataConverter.java
@@ -1,7 +1,11 @@
 package br.com.alura.TabelaFipe.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class DataConverter implements IDataConverter {
 
@@ -11,6 +15,17 @@ public class DataConverter implements IDataConverter {
     public <T> T getData(String json, Class<T> className) {
         try{
             return mapper.readValue(json, className);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> getList(String json, Class<T> className) {
+        try{
+            CollectionType list = mapper.getTypeFactory()
+                    .constructCollectionType(List.class, className)
+            return mapper.readValue(json, list);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
