dac6924a2bbde3805121f07e230640077f3e0aee
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-06-17 10:11:59 -0300

    modified: getList method included in IDataConverter interface

--- src/main/java/br/com/alura/TabelaFipe/service/IDataConverter.java
+++ src/main/java/br/com/alura/TabelaFipe/service/IDataConverter.java
@@ -1,5 +1,8 @@
 package br.com.alura.TabelaFipe.service;
 
+import java.util.List;
+
 public interface IDataConverter {
     <T> T getData(String json, Class<T> className);
+    <T> List<T> getList(String json, Class<T> className);
 }
